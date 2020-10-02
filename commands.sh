2a3f1198361e656e33ec4376d8b6b82eb8fe59d2
Tim Cranfield <tcranfield@electronet.co.nz>
2020-10-02 13:23:46 +1300

    Added python

--- commands.sh
+++ commands.sh
@@ -1,3 +1,13 @@
 export PATH=$PATH:~/go/bin
 
+cd ppdownlink
+
 protoc --go_out=plugins=grpc:. *.proto 
+
+#move generated file to ppdownlink
+
+python -m grpc_tools.protoc -I . --python_out=python --grpc_python_out=python *.proto
+
+cd ../ppuplink
+
+...
